5c519e14ab3ea396ae089e4279bef7bafa6d615e
Vijayant Katyal <1795103+vijayantkatyal@users.noreply.github.com>
2022-08-21 02:48:17 +0530

    [Added] rsuite ui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,97 @@
+import { useState } from 'react';
 import { Outlet } from "react-router-dom";
-import { NavBar } from './components/navbar';
+// import { NavBar } from './components/navbar';
+import { Container, Header, Sidebar, Sidenav, Content, Navbar, Nav } from 'rsuite';
+import CogIcon from '@rsuite/icons/legacy/Cog';
+import AngleLeftIcon from '@rsuite/icons/legacy/AngleLeft';
+import AngleRightIcon from '@rsuite/icons/legacy/AngleRight';
+import GearCircleIcon from '@rsuite/icons/legacy/GearCircle';
+import DashboardIcon from '@rsuite/icons/Dashboard';
+import GroupIcon from '@rsuite/icons/legacy/Group';
+import MagicIcon from '@rsuite/icons/legacy/Magic';
+
 import "./App.css";
 
-function App() {
-	return (
-		<div id="container" className="bg-white">
-			<NavBar/>
+import 'rsuite/dist/rsuite.min.css';
 
-			<main className="mt-24">
-			 	<Outlet/>
-			</main>
+
+const headerStyles = {
+	padding: 18,
+	fontSize: 16,
+	height: 56,
+	background: '#34c3ff',
+	color: ' #fff',
+	whiteSpace: 'nowrap',
+	overflow: 'hidden'
+};
+
+
+const NavToggle = ({ expand, onChange }) => {
+	return (
+		<Navbar appearance="subtle" className="nav-toggle">
+			<Nav>
+				<Nav.Menu
+					noCaret
+					placement="topStart"
+					trigger="click"
+					title={<CogIcon style={{ width: 20, height: 20 }} size="sm" />}
+				>
+					<Nav.Item>Help</Nav.Item>
+					<Nav.Item>Settings</Nav.Item>
+					<Nav.Item>Sign out</Nav.Item>
+				</Nav.Menu>
+			</Nav>
+
+			<Nav pullRight>
+				<Nav.Item onClick={onChange} style={{ width: 56, textAlign: 'center' }}>
+					{expand ? <AngleLeftIcon /> : <AngleRightIcon />}
+				</Nav.Item>
+			</Nav>
+		</Navbar>
+	);
+};
+
+function App() {
+	const [expand, setExpand] = useState(true);
+
+	return (
+		// <div id="container" className="bg-white">
+		// 	<NavBar/>
+
+		// 	<main className="mt-24">
+		// 	 	<Outlet/>
+		// 	</main>
+		// </div>
+
+		<div className="show-fake-browser sidebar-page">
+			<Container>
+				<Sidebar
+					style={{ display: 'flex', flexDirection: 'column', alignItems: 'stretch', alignSelf: 'flex-start' }}
+					width={expand ? 260 : 56}
+					// collapsible
+				>
+					<Sidenav expanded={expand} defaultOpenKeys={['3']} appearance="default">
+						<Sidenav.Header>
+							<div style={headerStyles}>Custom Sidenav</div>
+						</Sidenav.Header>
+						<Sidenav.Body>
+							<Nav>
+								<Nav.Item eventKey="1" active icon={<DashboardIcon />}>
+									Dashboard
+								</Nav.Item>
+								<Nav.Item eventKey="2" icon={<GroupIcon />}>
+									User Group
+								</Nav.Item>
+							</Nav>
+						</Sidenav.Body>
+					</Sidenav>
+				</Sidebar>
+
+				<Container>
+					<Outlet />
+				</Container>
+
+			</Container>
 		</div>
 	)
 }
--- src/screens/overview.js
+++ src/screens/overview.js
@@ -3,7 +3,8 @@ import { Fragment, useState } from 'react';
 export default function Overview() {
 	return (
 		<h1>
-			Hello
+			4
+Yep, Chrome has some issues, especially with nested flexboxes. For example I've got a nested flex box with children that have height:100% but they are rendering with natural height instead. And the weird thing is if I change their height to auto, then they render as height:100% like I was trying to do. It is definitely not intuitive if that's how it should work.
 		</h1>
 	)
 }
